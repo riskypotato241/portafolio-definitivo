04467a04086a8bdb63173c5869969f3a481ee021
Máximo Cáceres <maximocaceres2005@gmail.com>
2023-01-10 00:06:44 -0300

    12, error 404

---
+++ BackEnd/portfolio/src/main/java/com/portfolio/portfolio/Interface/IPersonaService.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portfolio.portfolio.Interface;
+
+import com.portfolio.portfolio.Entity.Persona;
+import java.util.List;
+
+/**
+ *
+ * @author maxim
+ */
+
+public interface IPersonaService {
+    public List<Persona> getPersona();
+    
+    public void savePersona(Persona persona);
+    
+    public void deletePersona(Long id);
+    
+    public Persona findPersona(Long id);
+}
