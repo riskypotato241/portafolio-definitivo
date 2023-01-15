2b762e482da560554047dd8dad1fe561e6d2f4aa
Máximo Cáceres <maximocaceres2005@gmail.com>
2023-01-15 07:06:49 -0300

    Add click function to login button in header.html

--- BackEnd/portfolio/src/main/java/com/portfolio/portfolio/Controller/PersonaController.java
+++ BackEnd/portfolio/src/main/java/com/portfolio/portfolio/Controller/PersonaController.java
@@ -8,6 +8,7 @@ import com.portfolio.portfolio.Entity.Persona;
 import com.portfolio.portfolio.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,9 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  *
  * @author maxim
- */
+ */ 
 @RestController
-public class PersonaController {
+@CrossOrigin(origins = "http://localhost:4200")
+public class PersonaController {    
     @Autowired IPersonaService ipersonaService;
     
     @GetMapping("personas/traer")
@@ -59,5 +61,10 @@ public class PersonaController {
         ipersonaService.savePersona(persona);
         return persona;
     }             
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona() {
+        return ipersonaService.findPersona((long)1);
+    }
     
 }
