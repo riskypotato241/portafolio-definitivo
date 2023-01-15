9f046147d1c1dcb1160d28d72447b5541de895fc
Máximo Cáceres <maximocaceres2005@gmail.com>
2023-01-15 06:24:57 -0300

    Add name from database

--- FrontEnd/portafolio/src/app/components/sobremi/sobremi.component.ts
+++ FrontEnd/portafolio/src/app/components/sobremi/sobremi.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { PersonaService } from './../../service/persona.service';
+import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
 
 @Component({
   selector: 'app-sobremi',
   templateUrl: './sobremi.component.html',
   styleUrls: ['./sobremi.component.css']
 })
-export class SobremiComponent {
+export class SobremiComponent implements OnInit  {
+  persona: persona = new persona("","","");
+
+  constructor(public personaService: PersonaService) { }
+
+  ngOnInit(): void { 
+    this.personaService.getPersona().subscribe(data => {this.persona = data})
+  }
 
 }
--- FrontEnd/portafolio/src/app/model/persona.model.ts
+++ FrontEnd/portafolio/src/app/model/persona.model.ts
@@ -1,10 +1,10 @@
 export class persona {
     id?: number;
-    nombre: string;
-    apellido: string;
-    img: string;
+    nombre: String;
+    apellido: String;
+    img: String;
 
-    constructor(nombre: string,apellido: string,img: string) {
+    constructor(nombre: String,apellido: String,img: String) {
         this.nombre = nombre;
         this.apellido =apellido;
         this.img = img;
