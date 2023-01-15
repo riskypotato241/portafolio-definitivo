59f2204cb259b7224c1cb8af55a3504967a83d35
Máximo Cáceres <maximocaceres2005@gmail.com>
2023-01-15 17:01:35 -0300

    Fix CORS policy error for login

--- BackEnd/portfolio/src/main/java/com/portfolio/portfolio/Security/Controller/AuthController.java
+++ BackEnd/portfolio/src/main/java/com/portfolio/portfolio/Security/Controller/AuthController.java
@@ -26,6 +26,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +38,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:4200") 
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
--- FrontEnd/portafolio/src/app/app.module.ts
+++ FrontEnd/portafolio/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { BannerComponent } from './components/banner/banner.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,6 +31,7 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
     
