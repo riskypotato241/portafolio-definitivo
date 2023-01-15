20b5b9767b298bd0eab9931a47f210e4a26f453e
Máximo Cáceres <maximocaceres2005@gmail.com>
2023-01-15 06:30:25 -0300

    Finish rooting

--- FrontEnd/portafolio/src/app/app.module.ts
+++ FrontEnd/portafolio/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { HardsoftskillsComponent } from './components/hardsoftskills/hardsoftski
 import { FooterComponent } from './components/footer/footer.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +22,9 @@ import { HttpClientModule } from '@angular/common/http';
     ExperienciaComponent,
     HardsoftskillsComponent,
     FooterComponent,
-    BannerComponent
+    BannerComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
